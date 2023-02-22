5faadcfc0d2c38ff751b6e92f2b920672c4cddd8
Abeera Tahir <abeeratahir2018@gmail.com>
2023-02-22 20:18:34 +0500

    Create custom methods for comments and likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :comments, class_name: "Comment", foreign_key: "post_id"
   has_many :likes, class_name: "Like", foreign_key: "post_id"
 
-  def update_post_counter
+  def update_posts_counter
     author.increment!(:posts_counter)
   end
 
