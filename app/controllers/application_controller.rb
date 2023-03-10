ee5ef61f19b5bcc2c4cad308cf0c734bda18af47
Abeera Tahir <abeeratahir2018@gmail.com>
2023-03-10 20:25:55 +0500

    Fix rubocop errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
+    redirect_to root_url, alert: exception.message
   end
 
   protected
